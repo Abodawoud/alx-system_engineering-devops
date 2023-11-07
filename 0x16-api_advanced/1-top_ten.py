4f387bcef0265a68342200670300d27c4dbf6a7a
Abodawoud <yousefabodawoud@gmail.com>
2023-11-07 17:53:28 +0200

    task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Using requests library"""
+import requests
+
+
+def top_ten(subreddit):
+    """Get Top ten from reddit API"""
+    api_url = f"https://www.reddit.com/r/{subreddit}/top.json?limit=10"
+
+    headers = {"User-Agent": "MyApp/1.0"}
+
+    response = requests.get(api_url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        for i in range(0, 10):
+            print(data["data"]["children"][i]["data"]["title"])
+    elif response.status_code == 302:
+        return 0
+    else:
+        return 0
