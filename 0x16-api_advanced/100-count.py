195e0c8f0591bdcbe8b0d0bb6fdbac83b5cec032
Abodawoud <yousefabodawoud@gmail.com>
2023-11-08 20:41:37 +0200

    task 100

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+"""requests module"""
+import requests
+
+
+def count_words(subreddit, word_list, after=None, dic=None):
+    """ returns a list containing the titles of all hot articles"""
+
+    if dic is None:
+        dic = {word: 0 for word in word_list}
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?after={after}"
+    res = requests.get(url, headers={'User-Agent': 'abeer'})
+
+    if res.status_code != 200:
+        return
+
+    hot = res.json()
+    for article in hot['data']['children']:
+        for word in word_list:
+            key_word = f" {word.lower()} "
+            title = article['data']['title'].lower()
+            dic[word] += key_word.count(title)
+
+    after = hot['data']['after']
+    if not after:
+        sorted_dict = dict(sorted(dic.items()))
+        for key, value in sorted_dict.items():
+            if value:
+                print(f"{key}: {value}")
+        return
+
+    return (count_words(subreddit, word_list, after, dic))
